2b12d75a33a8465e638e75ff447612857b35632c
ABrannigan <abran139@gmail.com>
2019-11-30 19:10:16 +0000

    set the correct path for static in settings

--- obclinic/obclinic/settings.py
+++ obclinic/obclinic/settings.py
@@ -152,7 +152,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/main/static/'
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
