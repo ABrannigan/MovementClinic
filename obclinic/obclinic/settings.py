03c6950c966816c4621e16fbf373d425872eeccd
ABrannigan <abran139@gmail.com>
2019-12-01 11:19:26 +0000

    changed static url back in settings

--- obclinic/obclinic/settings.py
+++ obclinic/obclinic/settings.py
@@ -152,7 +152,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/staticfiles/'
+STATIC_URL = '/static/'
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
